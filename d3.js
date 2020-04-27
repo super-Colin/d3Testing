89c966febf9691e9e20a1eb98d1509a1b374bd8e
superColin <supercolin.dev@gmail.com>
2020-04-27 07:23:23 -0400

    more about svg right now

--- d3.js
+++ d3.js
@@ -1,41 +1,41 @@
 
 
-// var width = Math.max(960, innerWidth),
-//     height = Math.max(500, innerHeight);
-let width = innerWidth;
-let height =innerHeight;
-
-var i = 0;
-
-var svg = d3.select("body").append("svg")
-    .style('background-color', '#222')
-    .attr("width", width)
-    .attr("height", height);
 
 
-svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
-function particle() {
-    var m = d3.mouse(this);
+function blackCircle(){
+    let width = 200;
+    let height = 200;
 
-    svg.insert("circle", "rect")
-        .attr("cx", m[0])
-        .attr("cy", m[1])
-        .attr("r", 1e-6)
-        .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
-        // .style("stroke", d3.hsl((i = (i + 1) % 360), 1, ((i % 10) * 0.1)))
-        .style("stroke-opacity", 1)
-        .transition()
-        .duration(3000)
-        .ease(Math.sqrt)
-        .attr("r", 200)
-        .style("stroke-opacity", 1e-6)
-        .remove();
 
-    d3.event.preventDefault();
+    const domCanvas = document.getElementById('circleCanvas');
+    const domContext = domCanvas.getContext('2d');
+    domContext.clearRect(0,0,width,height);
+
+    domContext.fillStyle = '#000';
+    domContext.beginPath();
+    let cx = width / 2;
+    let cy = height / 2;
+    let rad = width * 0.45;
+
+    domContext.arc(cx,cy, rad, 0, 2 * Math.PI, false);
+    domContext.fill();
+
 }
+
+blackCircle();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
