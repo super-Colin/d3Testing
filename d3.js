ef88f1e378e022664fb64bb3bdc27ee5f879a608
superColin <supercolin.dev@gmail.com>
2020-04-26 22:05:37 -0400

    some minor changes

--- d3.js
+++ d3.js
@@ -1,13 +1,17 @@
 
 
-var width = Math.max(960, innerWidth),
-    height = Math.max(500, innerHeight);
+// var width = Math.max(960, innerWidth),
+//     height = Math.max(500, innerHeight);
+let width = innerWidth;
+let height =innerHeight;
 
 var i = 0;
 
 var svg = d3.select("body").append("svg")
+    .style('background-color', '#222')
     .attr("width", width)
     .attr("height", height);
+
 
 svg.append("rect")
     .attr("width", width)
@@ -22,11 +26,12 @@ function particle() {
         .attr("cy", m[1])
         .attr("r", 1e-6)
         .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+        // .style("stroke", d3.hsl((i = (i + 1) % 360), 1, ((i % 10) * 0.1)))
         .style("stroke-opacity", 1)
         .transition()
-        .duration(2000)
+        .duration(3000)
         .ease(Math.sqrt)
-        .attr("r", 100)
+        .attr("r", 200)
         .style("stroke-opacity", 1e-6)
         .remove();
 
