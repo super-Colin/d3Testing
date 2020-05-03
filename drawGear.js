5c218a315cbf341019b8905b31caa5f714d96688
superColin <supercolin.dev@gmail.com>
2020-05-03 07:38:22 -0400

    working on things

---
+++ drawGear.js
@@ -0,0 +1,25 @@
+
+let gearContext = document.getElementById('drawGearCanvas').getContext('2d');
+let gearSvgElement = document.getElementById('drawGearSvg').getContext('2d');
+
+
+// 
+function createGearPath(radius, teethNumber, teethWholeDepth, annulusRatio = 0.25) {
+// 
+annulusRatio = annulusRatio % 1;
+
+
+// return
+
+}
+
+
+gearContext.append(createGearPath(10, 10, 5));
+
+
+
+
+
+
+
+
